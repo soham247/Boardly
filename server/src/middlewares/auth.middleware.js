d15aeba439388ae180c9de39a00bbeedd37d7855
soumojit-D48 <soumojitdas107@gmail.com>
2026-02-16 00:48:00 +0530

    Use AsyncHandler and Error middleware in other files

--- server/src/middlewares/auth.middleware.js
+++ server/src/middlewares/auth.middleware.js
@@ -1,28 +1,23 @@
 import { User } from "../models/user.model.js";
 import jwt from "jsonwebtoken";
+import { AppError } from "./error.middleware.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
-export const verifyJWT = async (req, res, next) => {
-    try {
-        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
+export const verifyJWT = asyncHandler(async (req, res, next) => {
+    const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "");
 
-        if (!token) {
-            return res.status(401).json({ message: "Unauthorized request" })
-        }
-
-        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
-
-        const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
-
-        if (!user) {
-            return res.status(401).json({ message: "Invalid access token" })
-        }
-
-        req.user = user;
-        next()
-    } catch (error) {
-        if (error.name === 'TokenExpiredError') {
-            return res.status(401).json({ message: "Access token expired" })
-        }
-        return res.status(401).json({ message: error?.message || "Invalid access token" })
+    if (!token) {
+        throw new AppError(401, "Unauthorized request");
     }
-}
+
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+    const user = await User.findById(decodedToken?._id).select("-password -refreshToken");
+
+    if (!user) {
+        throw new AppError(401, "Invalid access token");
+    }
+
+    req.user = user;
+    next();
+});
